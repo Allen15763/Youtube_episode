3785ed83eadfb6343b7783698b510b6e5d07f84f
Allen Li <love22798800@gmail.com>
2021-09-20 12:04:04 +0800

    output files

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,5 +1,9 @@
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.step import StepException
+from yt_concate.utils import Utils
+from yt_concate.pipeline.steps.postflight import Postflight
 
 from yt_concate.pipeline.pipeline import Pipeline
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA' # car
@@ -11,10 +15,14 @@ def main():
         'channel_id': CHANNEL_ID
     }
     steps = [
+        Preflight(),
         GetVideoList(),
+        DownloadCaptions(),
+        Postflight(),
     ]
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
+    p.run(inputs, utils)
 
 
 if __name__ == '__main__':
--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -3,3 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv('api_key')
+
+DOWNLOADS_DIR = 'downloads'
+VIDEOS_DIR = os.path.join(DOWNLOADS_DIR, "videos")
+CAPTIONS_DIR = os.path.join(DOWNLOADS_DIR, "captions")
