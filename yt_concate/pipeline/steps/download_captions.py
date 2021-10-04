3cf79e32cd61c7efc4383bae4ace17906c5275d5
Allen Li <love22798800@gmail.com>
2021-10-04 23:10:27 +0800

    Successfully downloads captions and save video list to file

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -2,21 +2,34 @@ from pytube import YouTube
 
 from .step import Step
 from .step import StepException
-
+import time
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
+        start = time.time()
         # download the package by: pip install pytube
         for url in data:
-            source = YouTube(url)
-            en_caption = source.captions.get_by_language_code('a.en')
-            en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            print('Downloading caption for', url)
+            if utils.caption_file_exists(url):
+                print('found existing caption file')
+                continue
 
-            print(en_caption_convert_to_srt)
+            try:
+                source = YouTube(url)
+                en_caption = source.captions.get_by_language_code('a.en')
+                en_caption_convert_to_srt = (en_caption.generate_srt_captions())
+            except (KeyError, AttributeError):
+                print('Error when downloading caption for', url)
+                continue
+
+            # print(en_caption_convert_to_srt)
 
             # save the caption to a file name Output.txt
-            text_file = open(utils.get_caption_path(url), "w")
+            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
             break
+
+        end = time.time()
+        print('took', end - start, 'second')
 
--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 api_key = os.getenv('api_key')
 
+
 DOWNLOADS_DIR = 'downloads'
 VIDEOS_DIR = os.path.join(DOWNLOADS_DIR, "videos")
 CAPTIONS_DIR = os.path.join(DOWNLOADS_DIR, "captions")
