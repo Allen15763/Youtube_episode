0e7dee03e3c86e4ccd05acad867d625e12ece537
Allen Li <love22798800@gmail.com>
2022-03-19 17:58:54 +0800

    create yt class to save url, caption and etc. And found for data. download video completed.

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -5,6 +5,8 @@ from yt_concate.pipeline.steps.read_caption import ReadCaptions
 from yt_concate.pipeline.steps.step import StepException
 from yt_concate.utils import Utils
 from yt_concate.pipeline.steps.postflight import Postflight
+from yt_concate.pipeline.steps.initialize_yt import InitializeYT
+from yt_concate.pipeline.steps.search import Search
 
 from yt_concate.pipeline.pipeline import Pipeline
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA' # car
@@ -13,13 +15,16 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA' # car
 
 def main():
     inputs = {
-        'channel_id': CHANNEL_ID
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
     }
     steps = [
         Preflight(),
         GetVideoList(),
+        InitializeYT(),
         DownloadCaptions(),
         ReadCaptions(),
+        Search(),
         Postflight(),
     ]
     utils = Utils()
--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,31 +1,33 @@
+import time
+
 from pytube import YouTube
 
 from .step import Step
 from .step import StepException
-import time
+
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         start = time.time()
         # download the package by: pip install pytube
-        for url in data:
-            print('Downloading caption for', url)
-            if utils.caption_file_exists(url):
+        for yt in data:
+            print('Downloading caption for', yt.id)
+            if utils.caption_file_exists(yt):
                 print('found existing caption file')
                 continue
 
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
             except (KeyError, AttributeError):
-                print('Error when downloading caption for', url)
+                print('Error when downloading caption for', yt.url)
                 continue
 
             # print(en_caption_convert_to_srt)
 
             # save the caption to a file name Output.txt
-            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file = open(utils.get_caption_filepath(yt.url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
             break
@@ -33,3 +35,4 @@ class DownloadCaptions(Step):
         end = time.time()
         print('took', end - start, 'second')
 
+        return data
